544bfc16c22fd6c7a5184b1f0a1a599ec5b5ec9b
feifeigd <feifeigd@21cn.com>
2016-11-22 23:27:24 +0800

    编译通过

---
+++ template/user/main.c
@@ -0,0 +1,118 @@
+/**
+  ******************************************************************************
+  * @file    main.c 
+  * @author  feifeigd
+  * @version V1.4.0
+  * @date    21-November-2016
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx.h"
+
+/** @addtogroup Template_Project
+  * @{
+  */ 
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+static __IO uint32_t uwTimingDelay;
+
+/* Private function prototypes -----------------------------------------------*/
+static void Delay(__IO uint32_t nTime);
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOF,ENABLE);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_Init(GPIOF, &GPIO_InitStructure);
+	
+  /*!< At this stage the microcontroller clock setting is already configured, 
+       this is done through SystemInit() function which is called from startup
+       files (startup_stm32f40_41xxx.s/startup_stm32f427_437xx.s/
+       startup_stm32f429_439xx.s/startup_stm32f401xx.s or startup_stm32f411xe.s)
+       before to branch to application main.
+       To reconfigure the default setting of SystemInit() function, 
+       refer to system_stm32f4xx.c file */
+
+
+  /* Add your application code here */
+  /* Insert 50 ms delay */
+  Delay(5);
+      
+  /* Infinite loop */
+  while (1)
+  {
+		GPIO_SetBits(GPIOF,GPIO_Pin_9 | GPIO_Pin_10);
+		Delay(5);
+		GPIO_ResetBits(GPIOF,GPIO_Pin_9 | GPIO_Pin_10);
+		Delay(5);
+  }
+}
+
+/**
+  * @brief  Inserts a delay time.
+  * @param  nTime: specifies the delay time length, in milliseconds.
+  * @retval None
+  */
+void Delay(__IO uint32_t nTime)
+{ 
+  uwTimingDelay = nTime;
+
+  while(uwTimingDelay != 0);
+}
+
+/**
+  * @brief  Decrements the TimingDelay variable.
+  * @param  None
+  * @retval None
+  */
+void TimingDelay_Decrement(void)
+{
+  if (uwTimingDelay != 0x00)
+  { 
+    uwTimingDelay--;
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */
