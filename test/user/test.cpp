d79f7fae8d31ae33b799cc6a0f6a937c15f6de14
feifeigd <feifeigd@21cn.com>
2017-02-23 00:23:59 +0800

    编译通过

--- test/user/test.cpp
+++ test/user/test.cpp
@@ -1,8 +1,19 @@
 
-#include <sys.h>
+extern "C"{
+#include <sys.h>	// Stm32_Clock_Init
+#include <delay.h>	// delay_init
+#include <usart.h>	// uart_init
+}
 
 int main(){
 	u8 t = 0;
-	while(true)++t;
+	Stm32_Clock_Init(336, 8, 2, 7);	// 初始化时钟为168Mhz
+	delay_init(168);	// 初始化延时函数
+	uart_init(84, 115200);	// 初始化串口为115200
+	while(true){
+		printf("t:%d\r\n", t);
+		delay_ms(500);
+		++t;
+	}
 	//return 0;
 }
